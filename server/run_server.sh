1ee1257645bb7b5d67b7bd343261df55abd4e4a1
Li Shouyang <lsylsy2@gmail.com>
2016-05-24 20:13:25 +0800

    Signed-off-by: Li Shouyang <lsylsy2@gmail.com>

--- server/run_server.sh
+++ server/run_server.sh
@@ -3,5 +3,4 @@ rm -r /tmp/gearmanserver
 rm -r /tmp/gearmandb
 mkdir -p /tmp/gearmanserver
 mkdir -p /tmp/gearmandb
-./deltacfs_client_gearman -o modules=subdir,subdir=/tmp/cloudsyncclient,big_writes /tmp/cloudsyncfuse -f
-umount /tmp/cloudsyncfuse
+./deltacfs_server_gearman
